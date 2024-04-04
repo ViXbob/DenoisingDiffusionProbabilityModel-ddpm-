f3b2335706e904fb037e11a610b20fdba885a4b4
ViXbob <vixbob@outlook.com>
2024-04-04 20:13:38 +0800

    update

--- Diffusion/Train.py
+++ Diffusion/Train.py
@@ -33,9 +33,15 @@ def train(modelConfig: Dict):
     # model setup
     net_model = UNet(T=modelConfig["T"], ch=modelConfig["channel"], ch_mult=modelConfig["channel_mult"], attn=modelConfig["attn"],
                      num_res_blocks=modelConfig["num_res_blocks"], dropout=modelConfig["dropout"]).to(device)
+    ema_update_gap = modelConfig["ema_update_gap"]
+    ema = ExponentialMovingAverage(net_model.parameters(), decay=0.9999)
     if modelConfig["training_load_weight"] is not None:
         net_model.load_state_dict(torch.load(os.path.join(
             modelConfig["save_weight_dir"], modelConfig["training_load_weight"]), map_location=device))
+        ema.load_state_dict(torch.load(os.path.join(
+            modelConfig["save_weight_dir"], "ema_" + modelConfig["training_load_weight"]), map_location=device))
+        print(f"Pretrained model weights loaded")
+    
     optimizer = torch.optim.AdamW(
         net_model.parameters(), lr=modelConfig["lr"], weight_decay=1e-4)
     cosineScheduler = optim.lr_scheduler.CosineAnnealingLR(
@@ -44,31 +50,20 @@ def train(modelConfig: Dict):
         optimizer=optimizer, multiplier=modelConfig["multiplier"], warm_epoch=modelConfig["epoch"] // 10, after_scheduler=cosineScheduler)
     trainer = GaussianDiffusionTrainer(
         net_model, modelConfig["beta_1"], modelConfig["beta_T"], modelConfig["T"]).to(device)
-
-    enable_ema =  modelConfig["enable_ema"]
-    if enable_ema:
-        ema_update_gap = modelConfig["ema_update_gap"]
-    else:
-        ema_update_gap = 1
     
-    ema = ExponentialMovingAverage(net_model.parameters(), decay=0.9999)
+    total_epoch = modelConfig["start_index"]
     
-    start_index = modelConfig["start_index"]
-    
-    print(f"enable_ema: {enable_ema}, ema_update_gap: {ema_update_gap}")
-    
-    # start training
-    total_epoch = start_index
     for e in range(modelConfig["epoch"]):
         total_epoch += 1
-        before_avg_loss = 0
-        len = 0
         
         if 'epoch_resume' in modelConfig and e < modelConfig['epoch_resume']:
             warmUpScheduler.step()
             continue
-                    
+        
         with tqdm(dataloader, dynamic_ncols=True) as tqdmDataLoader:
+            non_ema_loss = 0
+            len = 0
+            
             for images, labels in tqdmDataLoader:
                 # train
                 optimizer.zero_grad()
@@ -85,25 +80,43 @@ def train(modelConfig: Dict):
                     "LR": optimizer.state_dict()['param_groups'][0]["lr"]
                 })
                 
-                before_avg_loss += loss.item()
+                non_ema_loss += loss.item()
                 len += 1
                 
-                if enable_ema == True:
-                    if ema.num_updates % ema_update_gap == 0:
-                        ema.update()
-                        ema.copy_to()
+                if (ema.num_updates * (modelConfig['batch_size'] // 125)) % ema_update_gap == 0:
+                    ema.update()
+            
+            if total_epoch % 5 == 0:
+                torch.save(net_model.state_dict(), os.path.join(
+                    modelConfig["save_weight_dir"], 'ckpt_' + str(total_epoch) + "_.pt"))
+                torch.save(ema.state_dict(), os.path.join(
+                    modelConfig["save_weight_dir"], 'ema_ckpt_' + str(total_epoch) + "_.pt"))
+            
+            if total_epoch % 25 == 0:
+                torch.save(net_model.state_dict(), os.path.join(
+                    modelConfig["save_weight_dir"], 'iterations_ckpt_' + str(total_epoch // (10000 // len)) + "0k_.pt"))
+                torch.save(net_model.state_dict(), os.path.join(
+                    modelConfig["save_weight_dir"], 'ema_iterations_ckpt_' + str(total_epoch // (10000 // len)) + "0k_.pt"))
                 
-        print(f"before: {before_avg_loss / len}")
+                tqdmDataLoader.set_postfix(ordered_dict={})
+                
+                ema_loss = 0
+                non_ema_loss = 0
+                
+                for images, _ in tqdmDataLoader:
+                    x_0 = images.to(device)
+                    loss = trainer(x_0).sum() / 1000.
+                    non_ema_loss += loss.item()
+                    
+                    with ema.average_parameters():
+                        loss = trainer(x_0).sum() / 1000.
+                        ema_loss += loss.item()
+                
+                print(f"non-ema average loss: {non_ema_loss / len}, ema average loss: {ema_loss / len}")
+            else:
+                print(f"average loss: {non_ema_loss / len}")
         
         warmUpScheduler.step()
-        
-        torch.save(net_model.state_dict(), os.path.join(
-            modelConfig["save_weight_dir"], 'ckpt_' + str(start_index + e) + "_.pt"))
-        
-        if total_epoch % (10000 // len) == 0:
-            torch.save(net_model.state_dict(), os.path.join(
-                modelConfig["save_weight_dir"], 'iterations_ckpt_' + str(total_epoch // (10000 // len)) + "0k_.pt"))
-
 
 def eval(modelConfig: Dict):
     # load model and evaluate
@@ -127,6 +140,6 @@ def eval(modelConfig: Dict):
                 sampledImgs = sampledImgs * 0.5 + 0.5  # [0 ~ 1]
                 for j in range(modelConfig['batch_size']):
                     save_image(sampledImgs[j], os.path.join(
-                        modelConfig["sampled_dir"],  str(i * modelConfig["batch_size"] + j).zfill(6)) + ".png")
+                        modelConfig["sampled_dir"],  str(modelConfig['sampled_start_index'] + i * modelConfig["batch_size"] + j).zfill(6)) + ".png")
             else:
-                sampler.progressive_sampling_and_save(noisyImage, [str(os.path.join(modelConfig["sampled_dir"],  str(i * modelConfig["batch_size"] + j).zfill(6))) for j in range(modelConfig['batch_size'])], i)
+                sampler.progressive_sampling_and_save(noisyImage, [str(os.path.join(modelConfig["sampled_dir"],  str(modelConfig['sampled_start_index'] + i * modelConfig["batch_size"] + j).zfill(6))) for j in range(modelConfig['batch_size'])], i)
--- Main.py
+++ Main.py
@@ -49,9 +49,8 @@ if __name__ == '__main__':
     
     # Training arguments
     parser.add_argument('--start_index', type = int, help = 'The start index you store your checkpoints', required=False)
-    parser.add_argument('--enable_load_training_weight', action = 'store_true', help = 'Whether enable pre-trained training weights', required=False)
     parser.add_argument('--training_load_weight', type = str, help = 'The name of pretrained weights file', required=False)
-    parser.add_argument('--enable_ema', action = 'store_true', help = 'Whether enable EMA', required=False)
+    parser.add_argument('--enable_ema', action = 'store_true', help = 'Whether enable EMA replacing', required=False)
     parser.add_argument('--ema_update_gap', type = int, help = 'EMA update gap', required=False)
     parser.add_argument('--epoch', type = int, help = 'Total epoch to train', required=False)
     parser.add_argument('--epoch_resume', type = int, help = 'The epoch to resume', required=False)
@@ -62,6 +61,7 @@ if __name__ == '__main__':
     parser.add_argument('--enable_progress_sampling', action = 'store_true', help = 'Whether enable progressive sampling', required=False)
     parser.add_argument('--test_load_weight', type = str, help = 'File name of eval pre-trained weight', required=False)
     parser.add_argument('--sampled_dir', type = str, help = 'Directory of sampled imgs', required=False)
+    parser.add_argument('--sampled_start_index', type = int, help = 'The start index for sampled imgs', required=False)
     args = parser.parse_args()
     # End
     
@@ -100,11 +100,15 @@ if __name__ == '__main__':
             raise ValueError('Missing sampled_dir')
         else:
             model_config['sampled_dir'] = args.sampled_dir
+            
+        if args.sampled_start_index == None:
+            raise ValueError('Missing sampled_start_index')
+        else:
+            model_config['sampled_start_index'] = args.sampled_start_index
+
     else:
         if args.start_index == None:
             raise ValueError('Missing start_index')
-        if args.enable_load_training_weight == None:
-            raise ValueError('Missing enable_load_training_weight')
         if args.epoch == None:
             raise ValueError('Missing epoch')
 
@@ -112,20 +116,11 @@ if __name__ == '__main__':
         
         if args.enable_ema != None:
             model_config['enable_ema'] = args.enable_ema
-            if args.enable_ema == True:
-                if args.ema_update_gap != None or 'ema_update_gap' in model_config:
-                    if args.ema_update_gap != None:
-                        model_config['ema_update_gap'] = args.ema_update_gap
-                else:
-                    raise ValueError('Missing ema_update_gap when ema enabled')
+
+        if args.ema_update_gap != None:
+            model_config['ema_update_gap'] = args.ema_update_gaps
         
-        if args.enable_load_training_weight == True:
-            if args.training_load_weight == None:
-                raise ValueError('Missing ema_update_gap when ema enabled')
-            else:
-                model_config['training_load_weight'] = args.training_load_weight
-        else:
-            model_config['training_load_weight'] = None
+        model_config['training_load_weight'] = args.training_load_weight
 
         model_config['epoch'] = args.epoch
         
