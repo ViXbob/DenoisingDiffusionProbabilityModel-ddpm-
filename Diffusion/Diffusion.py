47e8c8493b2613682e9310df14534b447b1f9f41
ViXbob <vixbob@outlook.com>
2024-04-07 16:24:18 +0800

    update progressive sampling

--- Diffusion/Diffusion.py
+++ Diffusion/Diffusion.py
@@ -67,6 +67,9 @@ class GaussianDiffusionSampler(nn.Module):
         self.register_buffer('coeff2', self.coeff1 * (1. - alphas) / torch.sqrt(1. - alphas_bar))
 
         self.register_buffer('posterior_var', self.betas * (1. - alphas_bar_prev) / (1. - alphas_bar))
+        
+        self.register_buffer('sqrt_recip_alphas_cumprod', torch.sqrt(1. / alphas_bar))
+        self.register_buffer('sqrt_recipm1_alphas_cumprod', torch.sqrt(1. / alphas_bar - 1))
 
     def predict_xt_prev_mean_from_eps(self, x_t, t, eps):
         assert x_t.shape == eps.shape
@@ -75,7 +78,14 @@ class GaussianDiffusionSampler(nn.Module):
             extract(self.coeff2, t, x_t.shape) * eps
         )
 
-    def p_mean_variance(self, x_t, t):
+    def predict_x0_mean_from_eps(self, x_t, t, eps):
+        assert x_t.shape == eps.shape
+        return (
+            extract(self.sqrt_recip_alphas_cumprod, t, x_t.shape) * x_t - 
+            extract(self.sqrt_recipm1_alphas_cumprod, t, x_t.shape) * eps
+        )
+
+    def p_mean_variance(self, x_t, t, return_x0 = False):
         # below: only log_variance is used in the KL computations
         var = torch.cat([self.posterior_var[1:2], self.betas[1:]])
         var = extract(var, t, x_t.shape)
@@ -83,7 +93,10 @@ class GaussianDiffusionSampler(nn.Module):
         eps = self.model(x_t, t)
         xt_prev_mean = self.predict_xt_prev_mean_from_eps(x_t, t, eps=eps)
 
-        return xt_prev_mean, var
+        if return_x0 == False:
+            return xt_prev_mean, var
+        else:
+            return xt_prev_mean, var, self.predict_x0_mean_from_eps(x_t, t, eps)
 
     def forward(self, x_T, batch_id):
         """
@@ -120,7 +133,7 @@ class GaussianDiffusionSampler(nn.Module):
             for time_step_ in progress:
                 time_step = self.T - time_step_ - 1
                 t = x_t.new_ones([x_T.shape[0], ], dtype=torch.long) * time_step
-                mean, var= self.p_mean_variance(x_t=x_t, t=t)
+                mean, var, pred_x0 = self.p_mean_variance(x_t=x_t, t=t, return_x0 = True)
                 # no noise when t == 0
                 if time_step > 0:
                     noise = torch.randn_like(x_t)
@@ -131,7 +144,7 @@ class GaussianDiffusionSampler(nn.Module):
                 
                 if time_step % 50 == 0:
                     for index, path in enumerate(paths):
-                        save_image(torch.clamp(x_t[index] * 0.5 + 0.5, 0, 1), os.path.join(
+                        save_image(torch.clamp(pred_x0[index] * 0.5 + 0.5, 0, 1), os.path.join(
                             path,  str(20 - time_step // 50).zfill(2) + ".png"))
                 
 
