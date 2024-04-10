5090f5e32b4fff35306e451d940715d3d43fbab4
ViXbob <vixbob@outlook.com>
2024-04-10 21:05:31 +0800

    update init

--- Diffusion/__init__.py
+++ Diffusion/__init__.py
@@ -1,3 +1,5 @@
-from .Diffusion import *
+from .Diffusion import GaussianDiffusionSampler, GaussianDiffusionTrainer
 from .Model import *
 from .Train import *
+from .gaussian_diffusion import GaussianDiffusion
+from .script_util import create_gaussian_diffusion
